8d5eb4f330a57607b485928d8d5a56866d20a473
Angus Fenying <i.am.x.fenying@gmail.com>
2020-04-14 16:03:35 +0800

    add(global): added errors and linter ignores

--- src/lib/Server/index.ts
+++ src/lib/Server/index.ts
@@ -1,5 +1,6 @@
 export * from './Common';
 export * from './Server';
+export * from './Errors';
 export * from './SimpleRouter';
 export * from './Gateways/TCP';
 export * from './Gateways/Http';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,5 @@ export * from './Common';
 export * from './Encoder/Encoder';
 export * from './Encoder/Decoder';
 export * from './Server';
+export * from './Errors';
 export * from './Client';
